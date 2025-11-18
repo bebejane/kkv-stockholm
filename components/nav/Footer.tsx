2b471475b93e8061a09717532e1311d364c5c773
konst-teknik <information@konst-teknik.se>
2025-11-18 21:09:34 +0100

    Fix

--- components/nav/Footer.tsx
+++ components/nav/Footer.tsx
@@ -10,21 +10,22 @@ export function Footer({ footer }: FooterProps) {
 	return (
 		<footer className={s.footer}>
 			<h3>KKV Stockholm</h3>
-
-			<div className={s.support}>
-				<span>Med stöd av:</span>
-				<ul>
-					{footer?.support.map(({ name, logo }) => (
-						<li key={name}>
-							<img src={logo.url} alt={name} />
-						</li>
-					))}
-				</ul>
-			</div>
-			<div className={s.social}>
-				<span>Följ oss på:</span>
-				<Link href={footer.instagram}>Instagram</Link>
-				<Link href={footer.facebook}>Facebook</Link>
+			<div className={s.wrapper}>
+				<div className={s.support}>
+					<span>Med stöd av:</span>
+					<ul>
+						{footer?.support.map(({ name, logo }) => (
+							<li key={name}>
+								<img src={logo.url} alt={name} />
+							</li>
+						))}
+					</ul>
+				</div>
+				<div className={s.social}>
+					<span>Följ oss på: &nbsp;</span>
+					<Link href={footer.instagram}>Instagram</Link> &nbsp;
+					<Link href={footer.facebook}>Facebook</Link>
+				</div>
 			</div>
 		</footer>
 	);
