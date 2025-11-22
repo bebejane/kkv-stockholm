3a1ee6ff776c285603262802fdacd68f2ebdd5bf
Bebe Jane <bjornthief@gmail.com>
2025-11-22 22:42:25 +0100

    fix

--- app/(member)/api/member/route.ts
+++ app/(member)/api/member/route.ts
@@ -3,18 +3,16 @@ import { create } from '@/lib/controller/member';
 import { NextRequest, NextResponse } from 'next/server';
 
 export async function POST(req: NextRequest, ctx: RouteContext<'/api/member/report'>) {
-	return withMemberAuth(req, async (req, session) => {
-		try {
-			const data = await req.json();
-			const member = await create(data);
-			return new NextResponse(JSON.stringify(member), {
-				status: 200,
-				headers: { 'Content-Type': 'application/json' },
-			});
-		} catch (e) {
-			console.log(e);
-			const statusText = e instanceof Error ? e.message : (e as string);
-			return new NextResponse('error', { status: 500, statusText });
-		}
-	});
+	try {
+		const data = await req.json();
+		const member = await create(data);
+		return new NextResponse(JSON.stringify(member), {
+			status: 200,
+			headers: { 'Content-Type': 'application/json' },
+		});
+	} catch (e) {
+		console.log(e);
+		const statusText = e instanceof Error ? e.message : (e as string);
+		return new NextResponse('error', { status: 500, statusText });
+	}
 }
