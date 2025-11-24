55957f6424a547fa062ac5377358d7cc33fc7d37
Bebe Jane <bjornthief@gmail.com>
2025-11-24 21:13:33 +0100

    l

--- app/(datocms)/layout.tsx
+++ app/(datocms)/layout.tsx
@@ -1,11 +1,9 @@
 export default async function DatocmsLayout({ children }: LayoutProps<'/'>) {
 	return (
-		<>
-			<html>
-				<body id='root' style={{ margin: 0, padding: 0 }}>
-					{children}
-				</body>
-			</html>
-		</>
+		<html>
+			<body id='root' style={{ margin: 0, padding: 0 }}>
+				{children}
+			</body>
+		</html>
 	);
 }
