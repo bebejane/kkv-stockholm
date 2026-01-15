06d1d0676915224096d1628c4094f05dcc1a1d26
Bebe Jane <bjornthief@gmail.com>
2026-01-15 13:07:54 +0100

    blocks

--- components/content/blocks/index.ts
+++ components/content/blocks/index.ts
@@ -2,4 +2,4 @@ export { Image } from './Image';
 export { Staff } from './Staff';
 export { StaffItem } from './StaffItem';
 export { LinkWithImage } from './LinkWithImage';
-export { ImageWithLinkItem as LinkWithImageItem } from './ImageWithLinkItem';
+export { ImageWithLinkItem } from './ImageWithLinkItem';
