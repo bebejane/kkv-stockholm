410b0a453cb1c2d86cd4b187d33755b2fb40a361
Bebe Jane <bjornthief@gmail.com>
2026-02-13 20:54:07 +0100

    plugin

--- app/(datocms)/layout.tsx
+++ app/(datocms)/layout.tsx
@@ -1,9 +1,3 @@
 export default async function DatocmsLayout({ children }: LayoutProps<'/'>) {
-	return (
-		<html>
-			<body id='root' style={{ margin: 0, padding: 0 }}>
-				{children}
-			</body>
-		</html>
-	);
+	return <>{children}</>;
 }
--- app/(datocms)/plugin/pages/ReportPage.tsx
+++ app/(datocms)/plugin/pages/ReportPage.tsx
@@ -10,11 +10,13 @@ type PropTypes = {
 const start = new Date('2026-01-01');
 
 export function ReportPage({ ctx }: PropTypes) {
-	const end = endOfMonth(addMonths(new Date(), -1));
-	// Create an array of years with an array of months
+	const end = endOfMonth(addMonths(new Date(), 100));
 	const years = Array.from({ length: end.getFullYear() - start.getFullYear() + 1 }, (_, i) => ({
 		year: i + start.getFullYear(),
-		months: Array.from({ length: 12 }, (_, i) => i).slice(0, end.getMonth() + 2),
+		months: Array.from({ length: 12 }, (_, i) => i).slice(
+			0,
+			start.getFullYear() + i === end.getFullYear() ? end.getMonth() + 2 : 12,
+		),
 	}));
 
 	return (
@@ -24,17 +26,20 @@ export function ReportPage({ ctx }: PropTypes) {
 					<div key={year}>
 						<h3>{year}</h3>
 						<ul>
-							{months.map((month) => (
-								<li>
-									<a
-										download
-										key={month}
-										href={`/api/generate-report?date=${format(new Date(year, month, 1), 'yyyy-MM-dd')}`}
-									>
-										{format(new Date(year, month, 1), 'MMMM')}
-									</a>
-								</li>
-							))}
+							{months.map((month) => {
+								const date = new Date(year, month, 1);
+								return (
+									<li key={month}>
+										<a
+											key={month}
+											download={`KKV boknings rapport - ${format(date, 'MMMM (yyyy)')}.xlsx`}
+											href={`/api/generate-report?date=${format(date, 'yyyy-MM-dd')}`}
+										>
+											{format(date, 'MMMM')}
+										</a>
+									</li>
+								);
+							})}
 						</ul>
 					</div>
 				))}
