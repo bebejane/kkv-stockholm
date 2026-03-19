a06f2fcd12fb917945a952f10ff29f66a54e0f51
Bebe Jane <bjornthief@gmail.com>
2026-03-19 15:23:19 +0100

    p

--- components/content/blocks/StaffItem.tsx
+++ components/content/blocks/StaffItem.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import s from './StaffItem.module.scss';
-import { Image as DatoImage } from 'react-datocms';
 import { stripStega } from '@datocms/content-link';
 import Content from '../Content';
 
@@ -11,7 +10,7 @@ export type StaffItemBlockProps = {
 
 export function StaffItem(props: StaffItemBlockProps) {
 	const {
-		data: { image, text, _editingUrl },
+		data: { text, _editingUrl },
 	} = stripStega(props);
 	return (
 		<div
@@ -19,11 +18,6 @@ export function StaffItem(props: StaffItemBlockProps) {
 			data-datocms-content-link-group={true}
 			data-datocms-content-link-url={_editingUrl}
 		>
-			{image?.responsiveImage && (
-				<figure className={s.image}>
-					<DatoImage data={image.responsiveImage} intersectionMargin='0px 0px 200% 0px' />
-				</figure>
-			)}
 			{text && (
 				<div className={s.text} data-datocms-content-link-group={true}>
 					<Content content={text} />
